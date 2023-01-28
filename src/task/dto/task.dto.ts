0b4055857f43641a6aa708c2944983bde0b87d9d
mateocles <mat.bermudez@udla.edu.co>
2023-01-28 17:22:48 -0500

    initial project

---
+++ src/task/dto/task.dto.ts
@@ -0,0 +1,10 @@
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class TaskDTO {
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
+  @IsNotEmpty()
+  @IsBoolean()
+  readonly isDone: boolean;
+}
---
+++ src/task/task.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Validate } from 'class-validator';
+
+import { TaskDTO } from './dto/task.dto';
+import { TaskService } from './task.service';
+
+@Controller('api/v1/task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() taskDTO: TaskDTO) {
+    return this.taskService.create(taskDTO);
+  }
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() taskDTO: TaskDTO) {
+    return this.taskService.update(id, taskDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.delete(id);
+  }
+}
